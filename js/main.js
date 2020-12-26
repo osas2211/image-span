af6bc17e72b452062edc76c14cf676790d9ec2e5
osas2211 <osaretin.frank10@gmail.com>
2020-12-26 18:15:00 +0100

    Modified Javascript... Used Iternary Operators

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+/* CONDITIONING USING ITERNERY OPERATORS WITHIN AN ANNONYMOUS FOUNCTION */
+
+var onclick = function (selector){
+    for (let i = 0; i < selector.length; i++){
+        selector[i].addEventListener('click', () => {
+            (selector[i].className == 'bg1') ? selector[i].classList.replace('bg1','bg2')
+                :selector[i].classList.replace('bg2','bg1');
+        });
+    }
+};
+const bgChange = document.querySelectorAll('.grid div');
+onclick(bgChange)
+
+
+
+/* USING NORMAL CONDTIONING WITHIN A CLOSURE */
+/*
 (function onclick (){    
     const bgChange = document.querySelectorAll('.grid div');
     for (let i=0; i < bgChange.length; i++){
@@ -11,6 +28,6 @@
         });
     }
 })();
-
+*/
 
 
